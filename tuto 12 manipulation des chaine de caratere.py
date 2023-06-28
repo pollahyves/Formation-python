47bec5679be51d5985d17858ce3f47a5b446610b
pollah yves <yvesfranky961@gmail.com>
2023-06-28 06:36:45 +0100

    manipulation des chaine de caratere en python

---
+++ tuto 12 manipulation des chaine de caratere.py
@@ -0,0 +1,25 @@
+#BONSOIR ET BIENVENUE A TOUS DANS CE NOUVEAU TUTORIEL
+
+#TUTO 12  CHAÎNES DE CARACTÈRES
+
+"""
+str.upper(),srt.lower(),str.capitalize(),str.title()
+str.center(<largeur>,<caractere de remplisage>)
+str.find(<chaine>,<debut>,<fin>)
+str.index(<chaine>,<debut>,<fin>)
+str.strip()
+str.replace(<ancien>,<nouveau>,<occurence>)
+str.split(<separateur>)
+str.isupper(),str.islower(),isidentifier()
+"""
+phrase="je suis un etudiant"
+
+if "wisdom" in phrase:
+    print("mot present")
+else:
+    print("mot abscent")    
+
+
+
+
+
